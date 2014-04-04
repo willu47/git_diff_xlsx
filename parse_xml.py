17988c47294d85bb6bf9468d19565fb15ebd2727
Will Usher <w.usher@ucl.ac.uk>
2014-04-04 09:11:45 +0100

    Added author e-mail

--- parse_xml.py
+++ parse_xml.py
@@ -15,6 +15,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+You can contact the author via e-mail: w.usher[ot]ucl.ac.uk
 '''
 
 from lxml import etree
